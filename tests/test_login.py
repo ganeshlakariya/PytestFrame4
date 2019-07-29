b741333484dfde0dbfe160ed1f58e6ba9260406a
ganeshlakariya <ganeshlakariya@rediffmail.com>
2019-07-29 15:21:55 +0530

    Added folder Ganesh

--- tests/test_login.py
+++ tests/test_login.py
@@ -25,7 +25,6 @@ class TestLogin():
             ctime = moment.now().strftime("%d-%m-%Y_%H-%M-%S")
             filename = "screenshot_" + ctime
             allure.attach(self.driver.get_screenshot_as_png(),name=filename, attachment_type=allure.attachment_type.PNG)
-            self.driver.get_screenshot_as_file("C:/logs" + filename + ".png")
             raise
 
 
